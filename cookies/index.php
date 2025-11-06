f35b0536ee09675e48d50e81108da8e7876e92a1
Eduardo Batael <eduardobatael9@gmail.com>
2025-11-06 16:59:48 -0300

    cookies

---
+++ cookies/index.php
@@ -0,0 +1,28 @@
+<?php
+if (isset($_POST['nome'])) {
+    $venc = time() + (30 * 24 * 60 * 60); // 30 dias
+    setcookie("nome", $_POST['nome'], $venc);
+    header("Location: boasvindas.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <form method="post" action="">
+        <p>Qual é o seu nome?</p>
+        <input type="text" name="nome">
+        <button type="submit">Salvar</button>
+    </form>
+
+</body>
+
+</html>
