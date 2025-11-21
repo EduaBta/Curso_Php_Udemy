a8f55c7f760bd5b82dbeddd37aaa6e719c5ef012
Eduardo Batael <eduardobatael9@gmail.com>
2025-11-20 22:32:17 -0300

    curso de php

---
+++ sistemaBusca/conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = "localhost";
+$bancodedados = "carros";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+if ($mysqli->connect_errno) {
+    echo "falha na conexão: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
+}
